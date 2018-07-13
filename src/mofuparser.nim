c969cd9edb80c0c8d2a9c35e4db086d956c8c0bb
2vg <m0fqn090310@gmail.com>
2018-07-13 20:10:13 +0900

    token check

--- src/mofuparser.nim
+++ src/mofuparser.nim
@@ -256,7 +256,9 @@ proc mpParseHeader(headers: var array[headerSize, MPHeader], buf: var ptr char, 
         # HEADER VALUE CHECK
         var bufLen = bufLen - (buf - bufStart)
         start = buf
+        echo "befor: ", bufLen
         headerVector(buf, bufLen)
+        echo "after: ", bufLen
         while true:
           if buf[] == '\r' or buf[] == '\l':
             break
@@ -292,7 +294,7 @@ proc mpParseRequest*(mhr: MPHTTPReq, req: ptr char, reqLen: int): int =
       buf += 1
       break
     else:
-      if not URI_TOKEN[buf[].int].bool:
+      if not (buf[] > '\x1f' and buf[] < '\x7f'):
         return -1
     buf += 1
 
@@ -302,6 +304,7 @@ proc mpParseRequest*(mhr: MPHTTPReq, req: ptr char, reqLen: int): int =
   # PATH CHECK
   start = buf
   bufLen = bufLen - (buf - req)
+  echo bufLen
   urlVector(buf, bufLen)
   while true:
     if buf[] == ' ':
@@ -345,6 +348,7 @@ proc mpParseRequest*(mhr: MPHTTPReq, req: ptr char, reqLen: int): int =
 
   # PARSE HEADER
   bufLen = bufLen - (buf - req)
+  echo bufLen
   let hdrLen = mhr.headers.mpParseHeader(buf, bufLen)
 
   if hdrLen != -1:
